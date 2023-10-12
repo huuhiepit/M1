d986476270bc0858798ec38588f0d8888005cf32
Nguyen Huu Hiep <nhhiepit@gmail.com>
2023-10-12 14:22:35 +0700

    Bien, kieu du lieu va toan tu

--- js/practice/embed.js
+++ js/practice/embed.js
@@ -1,3 +1,10 @@
-function showMessage(){
-    alert('Hello World!!');
-}
+// function showMessage(){
+//     alert('Hello World!!');
+// }
+let inputWidth;
+let inputHeight;
+inputWidth = prompt('Enter the width');
+inputHeight = prompt('Enter the height');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+document.write('The area is: ' + width*height);
