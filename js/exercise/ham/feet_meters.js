b6f6bb8237c7fb26cb70e389d014772d859ee2dc
Nguyen Huu Hiep <nhhiepit@gmail.com>
2023-10-24 08:59:28 +0700

    bai tap ve ham

---
+++ js/exercise/ham/feet_meters.js
@@ -0,0 +1,9 @@
+function footToMetter(foot) {
+    return 0.305 * foot;
+}
+
+function meterToFoot(meter) {
+    return 3.279 * meter;
+}
+console.log(footToMetter(5).toFixed(3));
+console.log(meterToFoot(4).toFixed(3));
